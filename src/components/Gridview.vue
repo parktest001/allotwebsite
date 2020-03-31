c73373e9c10741c69b0d5d59975fa14392061bf3
kamalakannan2398 <62395051+kamalakannan2398@users.noreply.github.com>
2020-03-31 23:09:35 +0530

    New Feature and Carousel

--- src/components/Gridview.vue
+++ src/components/Gridview.vue
@@ -8,7 +8,26 @@
               <div> 
             <!-- <el-button @click="getImages()">Get Images</el-button> -->
             <div v-if="flag">
-              
+              <div style="height:500px;background-color:black;border:3px solid #5A487A;border-bottom:8px solid #5A487A;">
+              <center><span style="color:white;font-size:30px;padding-top:100px">Carousel</span></center>
+
+              <el-carousel style="height:380px;background-color:black;padding-top:80px" indicator-position="outside">
+                <el-carousel-item style="padding-top:0px;border-top:2px solid white;border-bottom:2px solid white" v-for="(key,val) in jsoninfo" :key="val">
+                <center><img :src="key.url" :class="imageview" style="height:400px;" > </center>               
+                </el-carousel-item>
+              </el-carousel>
+              </div>
+              <el-dropdown style="float:right;padding-right:20px;padding-top:10px" @command="handleSelect">
+                    <el-button type="primary">
+                      Sorting Type<i class="el-icon-arrow-down el-icon--right"></i>
+                    </el-button>
+                    <el-dropdown-menu slot="dropdown" >
+                      <el-dropdown-item command="1">Newest First</el-dropdown-item>
+                      <el-dropdown-item command="2">Oldest First</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+              <center><div style="padding-top:20px;font-size:20px;padding-left:160px">Sorting: {{sortingtypestring}}</div></center>
+               
               <div v-for="(key,value) in jsoninfo" :key="key.url" style="padding-top:20px">
                 <center>
               <el-card :body-style="{ padding: '0px' }" style="background-color: #F8F8FF;padding-top:20px;width:1000px;  border: 2px solid #5A487A;border-right: 8px solid #5A487A;border-left: 8px solid #5A487A;">
@@ -59,6 +78,9 @@ export default {
         description : "",
         flag : false,
         imgurlarr : "",
+        rev :false,
+        carouselimages : null,
+        sortingtypestring: "Newest First"
     };
   },
   computed: {
@@ -69,18 +91,12 @@ export default {
   },
   created()
   {
-                var self = this
-                this.flag=true
-                firebase.database().ref().once('value', function(snap){
-                self.jsoninfo=snap.val()
-                self.getSingleImage();
-                window.flag=true
-                })
+      this.recent()
   },
   methods: {
 
  
-    
+        
         getSingleImage()
         {
             // for(var val in this.jsoninfo){
@@ -111,6 +127,103 @@ export default {
                 //source.imgurlarr.push(url)                
 
         },
+        recent()
+        {
+                var self = this
+                this.flag=true
+                firebase.database().ref().once('value', function(snap){
+                //self.jsoninfo=snap.val()
+            
+                var newObject = {};
+                var keys = [];
+
+                for (var key in snap.val()) {
+                    keys.push(key);
+                }
+
+                for (var i = keys.length - 1; i >= 0; i--) {
+                    var value = snap.val()[keys[i]];
+                    newObject[keys[i]]= value;
+                }       
+
+                self.jsoninfo=newObject;
+            
+                self.getSingleImage();
+                window.flag=true
+                })
+        },
+        reverse()
+        {
+
+        },
+        old()
+        {
+          if(this.rev == false){
+          var newObject = {};
+                var keys = [];
+
+                for (var key in this.jsoninfo) {
+                    keys.push(key);
+                }
+
+                for (var i = keys.length - 1; i >= 0; i--) {
+                    var value = this.jsoninfo[keys[i]];
+                    newObject[keys[i]]= value;
+                }       
+
+                this.jsoninfo=newObject;
+                this.rev = true
+                this.sortingtypestring = "Oldest First"
+          }
+        },
+        newest()
+        {
+          if(this.rev == true){
+          var newObject = {};
+                var keys = [];
+
+                for (var key in this.jsoninfo) {
+                    keys.push(key);
+                }
+
+                for (var i = keys.length - 1; i >= 0; i--) {
+                    var value = this.jsoninfo[keys[i]];
+                    newObject[keys[i]]= value;
+                }       
+
+                this.jsoninfo=newObject;
+                this.rev=false
+                this.sortingtypestring = "Newest First"
+        }
+        },
+        handleSelect(index)
+        {
+            if(index == 1)
+            {
+                this.newest()
+            }
+            else
+            {
+                this.old()
+            }
+        },
     },
 };
 </script>
+<style>
+.el-carousel__item h3 {
+    color: #000000;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #ffffff;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #000000;
+  }
+</style>
