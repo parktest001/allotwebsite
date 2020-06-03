a17612d876b79971788ccda6fc43084a0accd674
kamalakannan2398 <62395051+kamalakannan2398@users.noreply.github.com>
2020-06-03 20:03:56 +0530

    https

--- server.js
+++ server.js
@@ -2,7 +2,33 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "./dist"));
+app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
+
+// // var history = require('connect-history-api-fallback');
+// // var connect = require('connect');
+
+// // var app = connect()
+// //   .use(history())
+// //   .listen(3000);
+// //   var express = require('express');
+
+// // var app = express();
+// // app.use(history());
+// // var middleware = history({});
+// // history({
+// //     index: '/index.html'
+// //   });
+// var express = require('express');
+// var path = require('path');
+// var serveStatic = require('serve-static');
+// app = express();
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname, '/dist'), function(err) {
+//       if (err) {
+//         res.status(500).send(err)
+//       }
+//     })
+//   })
