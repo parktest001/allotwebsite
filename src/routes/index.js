4deabde1ddc66fef1a15546b8728cd1097a380c1
kamalakannan2398 <62395051+kamalakannan2398@users.noreply.github.com>
2020-06-03 19:41:13 +0530

    https

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,13 +11,13 @@ const router = new Router({
     base: process.env.BASE_URL,
     routes: [
    
+    // {
+    //     path: '/',
+    //     name: 'home',
+    //     component: Home
+    // },
     {
-        path: '*',
-        name: 'home',
-        component: Home
-    },
-    {
-        path: '/Register',
+        path: '/',
         name: 'register',
         component: Register
     },
