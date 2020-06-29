e086fd5bb9262bb2a9ffd5fe3c2b0d46d8d9fc0e
2398kamalrkk <62395051+kamalakannan2398@users.noreply.github.com>
2020-06-29 18:04:11 +0530

    asdasd

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// var history = require('connect-history-api-fallback');
-// var express = require('express');
-// var path = require('path');
-// var serveStatic = require('serve-static');
-// app = express();
-// app.use(history());    
-// app.use(serveStatic(__dirname + "/dist"));
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// console.log('server started '+ port);
+var history = require('connect-history-api-fallback');
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+app.use(history());    
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
 
 // // var history = require('connect-history-api-fallback');
 // // var connect = require('connect');
@@ -60,6 +60,16 @@ const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/allotpark.buzz/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/allotpark.buzz/cert.pem')
 };
+
 var app  = https.createServer(options, function (req, res) {
     file.serve(req, res);
 }).listen(80);
+
+
+// var serveStatic = require('serve-static');
+// app = express();
+// app.use(history());    
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = process.env.PORT || 5000;
+// app.listen(port);
+// console.log('server started '+ port);
